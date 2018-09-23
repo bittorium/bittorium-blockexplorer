ec215ef7b9225a109c43ca26ac91f0f11a77063a
Bittorium Project <dev@bittorium.org>
2018-09-24 01:01:47 +0200

    Fix.

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 // settings
-const api = "https://bittorium.org/Blockexplorer";
+const api = "http://95.216.187.3:34916";
 const coinDifficultyTarget=175;
 const symbol="BTOR";
-const coinUnits=180000000;
+const coinUnits=100;
 const blockchainExplorer = "?hash={id}#block";
 const transactionExplorer = "?hash={id}#transaction";
 
 // poolList ["id","visible", "url", "api", "enabled"]
 const poolList = [
-    {"id":1,"visible":true,"url":"http://btor.simpool.xyz","api":"http://btor.simpool.xyz:4224","status":true,"flag":"US","loc":"USA"},
-    {"id":2,"visible":true,"url":"http://youpool.io/BTOR","api":"http://youpool.io/BTOR:3339","status":true,"flag":"FR","loc":"FRANCE"},
-    {"id":3,"visible":true,"url":"https://miningpool.studio/","api":"https://miningpool.studio:3333","status":true,"flag":"NL","loc":"NETHERLAND"},
+    {"id":1,"visible":true,"url":"http://btor.simpool.xyz","api":"http://btor.simpool.xyz:4117","status":true,"flag":"US","loc":"USA"},
+    {"id":2,"visible":true,"url":"http://youpool.io/BTOR","api":"http://118.178.122.224:8119","status":true,"flag":"FR","loc":"FRANCE"},
+    {"id":3,"visible":true,"url":"https://miningpool.studio/","api":"https://miningpool.studio:8119","status":true,"flag":"NL","loc":"NETHERLAND"},
 
 
 
 ];
 const seedsNodes = [
-    {"id":"rbx","url":"http://95.216.187.3:34916/"},
+    {"id":"rbx","url":"http://95.216.187.3:34916"},
     {"id":"mtl","url":"http://95.216.187.3:34916"},
     {"id":"sdn","url":"http://95.216.187.3:34916"},
 ];
