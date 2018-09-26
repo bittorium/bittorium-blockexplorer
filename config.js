fbfbe38e8d243fbc59d3ced2fc33881af9793437
Bittorium Project <dev@bittorium.org>
2018-09-26 22:49:29 +0200

    Add poolmining.us to pool list.

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ const poolList = [
     {"id":1,"visible":true,"url":"http://btor.simpool.xyz","api":"http://btor.simpool.xyz:4117","status":true,"flag":"US","loc":"USA"},
     {"id":2,"visible":true,"url":"http://youpool.io/BTOR","api":"http://118.178.122.224:8119","status":true,"flag":"FR","loc":"FRANCE"},
     {"id":3,"visible":true,"url":"https://miningpool.studio","api":"https://miningpool.studio:8119","status":true,"flag":"NL","loc":"NETHERLAND"},
+    {"id":4,"visible":true,"url":"https://poolmining.us","api":"https://poolmining.us:8119","status":true,"flag":"FR","loc":"FRANCE"},
 
 
 
