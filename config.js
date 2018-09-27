3b04c64a2db61c96650e0bd577c59fff3a48ea80
Bittorium Project <dev@bittorium.org>
2018-09-27 06:25:05 +0200

    Add SSL API for simpool.

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const transactionExplorer = "?hash={id}#transaction";
 
 // poolList ["id","visible", "url", "api", "enabled"]
 const poolList = [
-    {"id":1,"visible":true,"url":"http://btor.simpool.xyz","api":"http://btor.simpool.xyz:4117","status":true,"flag":"US","loc":"USA"},
+    {"id":1,"visible":true,"url":"http://btor.simpool.xyz","api":"https://btor.simpool.xyz:4119","status":true,"flag":"US","loc":"USA"},
     {"id":2,"visible":true,"url":"http://youpool.io/BTOR","api":"http://118.178.122.224:8119","status":true,"flag":"FR","loc":"FRANCE"},
     {"id":3,"visible":true,"url":"https://miningpool.studio","api":"https://miningpool.studio:8119","status":true,"flag":"NL","loc":"NETHERLAND"},
-    {"id":4,"visible":true,"url":"https://poolmining.us","api":"https://poolmining.us:8119","status":true,"flag":"FR","loc":"FRANCE"},
+    {"id":4,"visible":true,"url":"http://poolmining.us","api":"https://poolmining.us:8119","status":true,"flag":"FR","loc":"FRANCE"},
 
 
 
